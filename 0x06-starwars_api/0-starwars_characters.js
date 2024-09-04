b1bacbe4b97f62d822d184ddf2525379f07b8929
desniland <albertdesmond26@gmail.com>
2024-09-04 12:37:21 +0300

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,16 +2,16 @@
 
 const request = require('request');
 
-request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-    const actors = JSON.parse(body).characters;
-    exactOrder(actors, 0);
+request('"https://swapi-api.alx-tools.com/api/films/' + process.argv[2], function (err, res, body) {
+	 if (err) throw err;
+	  const actors = JSON.parse(body).characters;
+	  exactOrder(actors, 0);
 });
 const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
-  });
+	  if (x === actors.length) return;
+	  request(actors[x], function (err, res, body) {
+		      if (err) throw err;
+		      console.log(JSON.parse(body).name);
+		      exactOrder(actors, x + 1);
+		    });
 };
