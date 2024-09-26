6c9ae6f8064a6e5fd1300f9ccfac4974acfe8cf2
desniland <albertdesmond26@gmail.com>
2024-09-26 11:51:50 +0300

    Finds Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,6 +3,7 @@
 """Gets Island Perimeter
 """
 
+
 def island_perimeter(grid):
     """
     Calculates the perimeter of the island described in a grid
@@ -20,7 +21,7 @@ def island_perimeter(grid):
                     P += 1
                 if (x >= len(grid) - 1 or grid[x + 1][y] == 0):
                     P += 1
-                if (y <= 0 or grid[x][y -1] == 0):
+                if (y <= 0 or grid[x][y - 1] == 0):
                     P += 1
                 if (y >= len(grid[x]) - 1 or grid[x][y + 1] == 0):
                     P += 1
